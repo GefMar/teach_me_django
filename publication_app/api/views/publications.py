6c452aed7767a64f785fc347f605cbbaf1ee1283
SARomanchuk <SARomanchuk@sberbank.ru>
2021-12-21 21:03:45 +0300

    DRF

--- publication_app/api/views/publications.py
+++ publication_app/api/views/publications.py
@@ -1,13 +1,31 @@
 from rest_framework import filters
-from rest_framework.mixins import ListModelMixin, CreateModelMixin
+from django.db.models import F, Sum, Q, Count
+from rest_framework import permissions
+from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin
 from rest_framework.viewsets import GenericViewSet
 
 from ..serializers.publications import PostSerializer
 from ...models import Post
 
 
-class PostsViewSet(GenericViewSet, ListModelMixin, CreateModelMixin):
+class TestPermission(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        return True
+
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            return True
+        return False
+
+
+class PostsViewSet(GenericViewSet, ListModelMixin, CreateModelMixin, RetrieveModelMixin):
+    permission_classes = [TestPermission, ]
     serializer_class = PostSerializer
     queryset = Post.objects.filter(is_public=True)
     filter_backends = [filters.OrderingFilter, ]
     ordering_fields = ['create_date', ]
+
+    def list(self, request, *args, **kwargs):
+        # Post.objects.annotate(likes_count=Count(F('likes'))).order_by(-F("likes_count"))
+        print(1)
--- teach_me/settings.py
+++ teach_me/settings.py
@@ -138,7 +138,6 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 20,
     "MAX_PAGE_SIZE": 100,
-
 }
 
 SPECTACULAR_SETTINGS = {
